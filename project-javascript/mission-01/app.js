a5577f548b00a4270b05b55aa036aefbbe4d11e3
Hailey Kim <kfbkhw@gmail.com>
2023-12-28 11:38:53 +0900

    finished mission 1

--- project-javascript/mission-01/app.js
+++ project-javascript/mission-01/app.js
@@ -1,1 +1,23 @@
-// do something!
+// if the key 'navActive' does not exist
+if (localStorage.getItem('navActive') === null) {
+    localStorage.navActive = 'n';
+}
+
+// when the page is loaded
+if (localStorage.navActive === 'y') {
+    document.querySelector('nav').classList.add('active');
+}
+document.querySelector('body').style.visibility = 'visible';
+
+// when the toggle button clicks
+document.querySelector('i.toggle').addEventListener('click', () => {
+    document.querySelector('body').classList.remove('preload');
+    if (localStorage.navActive === 'y') {
+        document.querySelector('nav').classList.remove('active');
+        localStorage.navActive = 'n';
+    }
+    else {
+        document.querySelector('nav').classList.add('active');
+        localStorage.navActive = 'y';
+    }
+});
