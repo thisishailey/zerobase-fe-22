ba38b52acce69f2220c6c64ce84db46702771be2
Hailey Kim <kfbkhw@gmail.com>
2023-12-29 16:58:27 +0900

    made time and hand divs

--- project-javascript/mission-02/AnalogClock.js
+++ project-javascript/mission-02/AnalogClock.js
@@ -1,5 +1,50 @@
 const AnalogClock = $container => {
-  // do something!
+  createTimeDiv($container);
+  createHandDiv($container);
 };
+
+
+// create time div
+
+function createTimeDiv(clock) {
+  for (let i = 1; i < 13; i++) {
+    const newDiv = document.createElement('div');
+    styleTimeContainer(newDiv);
+    newDiv.classList.add('time', `time${i}`);
+    const newTime = document.createElement('div');
+    styleTimeElement(newTime);
+    newDiv.appendChild(newTime);
+    clock.appendChild(newDiv);
+  }
+}
+
+function styleTimeContainer(timeContainer) {
+  timeContainer.style.display = 'flex';
+  timeContainer.style.flexDirection = 'column';
+  timeContainer.style.alignItems = 'center';
+}
+
+function styleTimeElement(timeElement) {
+  timeElement.style.width = '2px';
+  timeElement.style.height = '20px';
+  timeElement.style.marginTop = '5px';
+  timeElement.style.background = 'black';
+}
+
+
+// create hand div
+
+const handClass = ['hour', 'minute', 'second']
+
+function createHandDiv(clock) {
+  for (let i = 0; i < 3; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('hand');
+    newDiv.classList.add(handClass[i]);
+    clock.appendChild(newDiv);
+  }
+}
+
+
 
 export default AnalogClock;
