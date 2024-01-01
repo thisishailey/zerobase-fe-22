93f9eac96f6100ec73d8570b09131e7cd6a2cd74
Hailey Kim <kfbkhw@gmail.com>
2024-01-01 17:53:50 +0900

    finished mission 3 (add star color change functions, event listeners, custom event)

--- project-javascript/mission-03/star-rating/Star.js
+++ project-javascript/mission-03/star-rating/Star.js
@@ -5,3 +5,46 @@ export function createStarIcon(ratingContainer, starNum) {
         ratingContainer.appendChild(starIcon);
     }
 }
+
+export function starChangeColor(container) {
+    container.querySelectorAll('i.bxs-star').forEach((star) => {
+        hover(star);
+        unhover(star);
+        click(star, container);
+    });
+}
+
+function hover(star) {
+    star.addEventListener('mouseover', (e) => {
+        colorChange(e, 'hovered');
+    });
+}
+
+function unhover(star) {
+    star.addEventListener('mouseleave', () => {
+        document.querySelectorAll('i.hovered').forEach((e) => {
+            e.classList.remove('hovered');
+        });
+    });
+}
+
+function click(star, container) {
+    star.addEventListener('click', (e) => {
+        colorChange(e, 'selected');
+        const rating = container.querySelectorAll('i.selected').length;
+        const event = new CustomEvent('rating-change', { detail: rating });
+        container.dispatchEvent(event);
+    });
+}
+
+function colorChange(e, color) {
+    const colorStars = [e.target];
+    let prevStar = e.target.previousElementSibling;
+    while (prevStar) {
+        colorStars.push(prevStar);
+        prevStar = prevStar.previousElementSibling;
+    }
+    colorStars.forEach((colorStar) => {
+        colorStar.classList.add(color);
+    });
+}
--- project-javascript/mission-03/star-rating/index.js
+++ project-javascript/mission-03/star-rating/index.js
@@ -1,11 +1,12 @@
 import { addStyleSheet } from "./style.js";
 import { createStarRatingContainer } from "./Container.js";
-import { createStarIcon } from "./Star.js";
+import { createStarIcon, starChangeColor } from "./Star.js";
 
 const StarRating = (container) => {
     addStyleSheet();
     const starNum = container.getAttribute('data-max-rating');
     createStarIcon(createStarRatingContainer(container), starNum);
+    starChangeColor(container);
 }
 
 export default StarRating;
