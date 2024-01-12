7913c65d2b0595e14ce91b4eaf637be05d50e69c
Hailey Kim <kfbkhw@gmail.com>
2024-01-12 20:44:15 +0900

    solved one basic algorithm problem

---
+++ algorithm/basic/B01-switch.js
@@ -0,0 +1,18 @@
+const n = 3; // input n will be in range of 0 to 6 (Sun - Sat)
+
+function getDay(n) {
+    let day = '';
+    switch (n) {
+        case 0: day = 'Sunday'; break;
+        case 1: day = 'Monday'; break;
+        case 2: day = 'Tuesday'; break;
+        case 3: day = 'Wednesday'; break;
+        case 4: day = 'Thursday'; break;
+        case 5: day = 'Friday'; break;
+        case 6: day = 'Saturday'; break;
+        default: day = new Error("Input is invalid.");
+    }
+    return day;
+}
+
+console.log(getDay(n));
