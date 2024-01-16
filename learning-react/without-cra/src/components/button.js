c836c160b8e1020611b2edfe3c2e2b9b2cd746bb
Hailey Kim <kfbkhw@gmail.com>
2024-01-16 15:38:07 +0900

    finished making a button with click event using react, babel, webpack

--- learning-react/without-cra/src/components/button.js
+++ learning-react/without-cra/src/components/button.js
@@ -1,8 +1,39 @@
+import React from "react";
+
 export default function Button() {
     const [key, setKey] = React.useState(true);
+
+    const btnStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '200px',
+        height: '100px',
+        margin: '200px auto',
+        borderRadius: '10px',
+        fontFamily: 'Verdana, sans-serif',
+        fontSize: '2rem',
+        userSelect: 'none',
+        webkitUserSelect: 'none',
+        cursor: 'pointer',
+    }
+
+    const clickBtnStyle = {
+        ...btnStyle,
+        background: '#86B6F6',
+        color: '#EEF5FF',
+    }
+
+    const clickedBtnStyle = {
+        ...btnStyle,
+        background: '#EEF5FF',
+        color: '#86B6F6',
+        border: '1px solid #86B6F6',
+    }
+
     return (
-        <div onClick={() => setKey(!key)}>
-            {key ? "click" : "clicked"}
+        <div onClick={() => setKey(!key)} style={key ? clickBtnStyle : clickedBtnStyle}>
+            {key ? "click" : "clicked!"}
         </div>
     );
 }
