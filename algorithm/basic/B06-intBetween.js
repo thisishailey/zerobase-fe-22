7667433e0f4e8fa94901bc8113231ea2a756d354
Hailey Kim <kfbkhw@gmail.com>
2024-01-18 16:07:23 +0900

    solved problem B6

--- algorithm/basic/B06-intBetween.js
+++ algorithm/basic/B06-intBetween.js
@@ -1,31 +1,22 @@
 /*** 2. 두 수 사이 숫자 ***/
 
-/* user code */
+/* my solution */
 function answer(x, y) {
   let result = [];
-
-  if (x > y) {
-    let t = x;
-    x = y;
-    y = t;
-  }
-
-  for (let i = x; i <= y; i++) {
-    result.push(i);
-  }
-
+  for (let i = Math.min(x, y); i <= Math.max(x, y); i++) result.push(i);
   return result;
 }
 
-/* main code */
+/* test case */
 let input = [
-  // TC: 1
+  // TC-1 output=[ 3, 4, 5, 6, 7 ]
   [3, 7],
-  // TC: 2
+  // TC-2 output=[ 3, 4, 5, 6, 7, 8 ]
   [8, 3],
-  // TC: 3
+  // TC-3 output=[ 10, 11, 12 ]
   [12, 10],
 ];
+
 for (let i = 0; i < input.length; i++) {
   process.stdout.write(`#${i + 1} `);
   console.log(answer(input[i][0], input[i][1]));
