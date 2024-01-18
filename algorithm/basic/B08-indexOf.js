9b8e49a7ef6f7a54d0ae4f1211b74093f211d481
Hailey Kim <kfbkhw@gmail.com>
2024-01-18 16:24:25 +0900

    solved problem B8

--- algorithm/basic/B08-indexOf.js
+++ algorithm/basic/B08-indexOf.js
@@ -1,27 +1,22 @@
 /*** 4. 헨드폰 판매 ***/
 
-/* user code */
+/* my solution */
 function answer(employee) {
-  let employee_id;
-
-  // 코드 구현 시작 영역
-
-  // …
-
-  // 코드 구현 종료 영역
-
-  return average;
+  const maxVal = Math.max(...employee);
+  const result = employee.indexOf(maxVal) + 1;
+  return result;
 }
 
-/* main code */
+/* test case */
 let input = [
-  // TC: 1
+  // TC-1 output=3
   [3, 7, 9, 6, 1],
-  // TC: 2
+  // TC-2 output=2
   [2, 7, 1, 4, 3, 0, 5],
-  // TC: 3
+  // TC-3 output=6
   [7, 5, 0, 1, 2, 12, 6],
 ];
+
 for (let i = 0; i < input.length; i++) {
   console.log(`#${i + 1} ${answer(input[i])}`);
 }
