4dbf7ddc25200b52752a85ca575cdb7a6757c016
Hailey Kim <kfbkhw@gmail.com>
2024-01-21 21:11:27 +0900

    update cartToggleButton

--- js-market-service/module/cartToggleButton.js
+++ js-market-service/module/cartToggleButton.js
@@ -3,9 +3,20 @@ import CART_COOKIE_KEY from "../constants/cart.js";
 
 const getCartToggleButton = (parentNode, productInfo) => {
     const cartButton = createCustomElement('button', {
-        className: 'cart-toggle-btn', type: 'button', onclick: () => { addCartInfo(productInfo) }
+        className: 'cart-toggle-btn', type: 'button', onclick: () => {
+            if (isInCart(productInfo)) {
+                cartImage.src = './public/assets/cart.png';
+                removeCartInfo(productInfo);
+            }
+            else {
+                cartImage.src = './public/assets/cartDisabled.png';
+                addCartInfo(productInfo);
+            }
+        }
     }, parentNode);
-    createCustomElement('img', { className: 'cart-image', src: './public/assets/cart.png' }, cartButton);
+    const cartImage = createCustomElement('img', {
+        className: 'cart-image', src: isInCart(productInfo) ? './public/assets/cartDisabled.png' : './public/assets/cart.png'
+    }, cartButton);
 }
 
 const addCartInfo = (productInfo) => {
@@ -17,6 +28,21 @@ const addCartInfo = (productInfo) => {
         ...originalCookie,
         productInfo
     ]));
+}
+
+const removeCartInfo = ({ id }) => {
+    const originalCookie = JSON.parse(localStorage.getItem(CART_COOKIE_KEY));
+
+    originalCookie.splice(originalCookie.findIndex(e => e.id === id), 1);
+
+    localStorage.setItem(CART_COOKIE_KEY, JSON.stringify([
+        ...originalCookie
+    ]));
+}
+
+const isInCart = ({ id }) => {
+    const originalCookie = JSON.parse(localStorage.getItem(CART_COOKIE_KEY)) || [];
+    return originalCookie.some(e => e.id === id);
 }
 
 export { getCartToggleButton }
