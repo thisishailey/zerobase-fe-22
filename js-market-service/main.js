1637e752d6fb61e7de75c1c85747905f6a8b66a9
Hailey Kim <kfbkhw@gmail.com>
2024-01-22 09:15:28 +0900

    finished index.html

--- js-market-service/main.js
+++ js-market-service/main.js
@@ -2,33 +2,12 @@
 
 import getProductSection from "./module/productSection.js";
 
-getProductSection(
-    '인기 상품',
-    [
-        {
-            "id": 1,
-            "imgSrc": "./public/assets/paprika.jpg",
-            "name": "파프리카 2입",
-            "discountPercent": 20,
-            "price": 2000,
-            "originalPrice": 2500
-        },
-        {
-            "id": 2,
-            "imgSrc": "./public/assets/carrot.jpg",
-            "name": "친환경 당근 400g",
-            "discountPercent": 33,
-            "price": 2010,
-            "originalPrice": 3000
-        },
-        {
-            "id": 3,
-            "imgSrc": "./public/assets/pumpkin.jpg",
-            "name": "단호박 1통",
-            "discountPercent": 12,
-            "price": 3520,
-            "originalPrice": 4000
-        }
-    ],
-    document.body
-);
+try {
+    const res = await fetch('./public/mock/sectionListData.json');
+    const data = await res.json();
+    data.sectionInfoList.forEach(e => {
+        getProductSection(e.sectionTitle, e.productList, document.body);
+    });
+} catch (error) {
+    console.log(error);
+}
