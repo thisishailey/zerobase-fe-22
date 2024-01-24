84e8f60511a640e60fc31bab738570a64f18c767
Hailey Kim <kfbkhw@gmail.com>
2024-01-24 12:12:06 +0900

    finished the share section

--- js-event-page/module/shareEvent.js
+++ js-event-page/module/shareEvent.js
@@ -1,5 +1,9 @@
-const setShareEvent = () => {
+const shareButton = document.querySelector('#url-share-button');
 
+const setShareEvent = () => {
+    shareButton.onclick = () => {
+        navigator.clipboard.writeText(location.href);
+    }
 }
 
 export default setShareEvent;
