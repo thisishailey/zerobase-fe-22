015b61dd1e93c2dc5a50bc9e914be3e15765e096
Hailey Kim <kfbkhw@gmail.com>
2024-02-01 16:49:25 +0900

    finished modal project

--- js-100-master/case11_modal_popup/modal/script.js
+++ js-100-master/case11_modal_popup/modal/script.js
@@ -1,6 +1,61 @@
-;(function () {
-  'use strict'
-  const get = (target) => {
-    return document.querySelector(target)
+; (function () {
+  'use strict';
+
+  // util
+
+  const get = (target) => document.querySelector(target);
+
+  // constants
+
+  const $modalButton = get('.modal_open_button');
+  const $body = get('body');
+  const $modal = get('.modal');
+
+  // modal open
+
+  const setModalOpenEvent = () => {
+    $modalButton.addEventListener('click', () => {
+      $body.classList.add('scroll_lock');
+      $modal.classList.add('show');
+    });
   }
-})()
+
+  // modal close
+
+  const setModalCloseEvent = () => {
+    document.addEventListener('click', (e) => {
+      if (!get('.modal.show')) return;
+
+      if (e.target.classList.contains('confirm')) {
+        modalConfirmed();
+      } else if (e.target.classList.contains('cancel')) {
+        modalCanceled();
+      } else if (e.target !== $modal) {
+        return;
+      }
+
+      $body.classList.remove('scroll_lock');
+      $modal.classList.remove('show');
+    });
+  }
+
+  // modal confirmed/canceled action
+
+  const modalConfirmed = () => {
+    console.log('confirmed!');
+  }
+
+  const modalCanceled = () => {
+    console.log('canceled...');
+  }
+
+  // init
+
+  const init = () => {
+    setModalOpenEvent();
+    setModalCloseEvent();
+  }
+
+  init();
+
+})();
