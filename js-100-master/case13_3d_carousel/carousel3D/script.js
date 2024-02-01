c60b757cda49352aeeb63acbb5ba80f58adf8555
Hailey Kim <kfbkhw@gmail.com>
2024-02-01 22:13:19 +0900

    starting 3d carousel project

---
+++ js-100-master/case13_3d_carousel/carousel3D/script.js
@@ -0,0 +1,21 @@
+; (function () {
+  'use strict';
+
+  // util
+
+  const get = (target) => document.querySelector(target);
+
+  // constants
+
+  const $prevButton = get('.prev_button');
+  const $nextButton = get('.next_button');
+
+  // init
+
+  const init = () => {
+
+  }
+
+  init();
+
+})();
