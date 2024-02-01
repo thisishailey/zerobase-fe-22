f306b5a8ff80d522735c9ff752da3ae768020eb3
Hailey Kim <kfbkhw@gmail.com>
2024-02-01 16:53:37 +0900

    starting stopwatch project

---
+++ js-100-master/case12_stopwatch/stopwatch/script.js
@@ -0,0 +1,19 @@
+; (function () {
+  'use strict';
+
+  // util
+
+  const get = (target) => document.querySelector(target);
+
+  // constants
+
+
+  // init
+
+  const init = () => {
+
+  }
+
+  init();
+
+})();
