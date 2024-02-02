3c51b5f457513bb557fd89b62155634366438b4f
Hailey Kim <kfbkhw@gmail.com>
2024-02-02 10:54:49 +0900

    finish 3d carousel project

--- js-100-master/case13_3d_carousel/carousel3D/script.js
+++ js-100-master/case13_3d_carousel/carousel3D/script.js
@@ -7,13 +7,52 @@
 
   // constants
 
+  const $carousel = get('.carousel');
   const $prevButton = get('.prev_button');
   const $nextButton = get('.next_button');
+
+  // carousel class
+
+  class Carousel {
+    constructor(element) {
+      this.element = element;
+      this.cellList = element.querySelectorAll('.carousel_cell');
+      this.width = element.parentNode.offsetWidth;
+      this.cellCount = this.cellList.length;
+      this.current = 0;
+      this.yAngle = 360 / this.cellCount;
+      this.zAxis = Math.round((this.width / 2) / (Math.tan(Math.PI / this.cellCount)));
+    }
+
+    setCarousel() {
+      this.cellList.forEach((e, i) => {
+        e.style.transform = `rotateY(${this.yAngle * i}deg) translateZ(${this.zAxis}px)`;
+      })
+    }
+
+    setRotateCarouselEvent(prev, next) {
+      prev.addEventListener('click', () => {
+        this.current--;
+        this.rotateCarousel();
+      });
+
+      next.addEventListener('click', () => {
+        this.current++;
+        this.rotateCarousel();
+      });
+    }
+
+    rotateCarousel() {
+      this.element.style.transform = `translateZ(${this.zAxis * -1}px) rotateY(${this.yAngle * this.current * -1}deg)`;
+    }
+  }
 
   // init
 
   const init = () => {
-
+    const carousel = new Carousel($carousel);
+    carousel.setCarousel();
+    carousel.setRotateCarouselEvent($prevButton, $nextButton);
   }
 
   init();
