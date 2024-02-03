2c2c990efe24028204cb3ca319900e21f5ade8c0
Hailey Kim <kfbkhw@gmail.com>
2024-02-03 18:52:57 +0900

    working on random card draw project

--- learning-react/random-card-draw/src/App.tsx
+++ learning-react/random-card-draw/src/App.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import data from './data/cards';
+import BusinessCard from './component/BusinessCard';
 
-function App() {
+type CardData = typeof data[0];
+
+export default function App() {
+  const [cards, setCards] = useState<CardData[]>([])
+  const [drawnCards, setDrawnCards] = useState<CardData[]>([])
+
+  useEffect(() => {
+    setCards(data)
+  }, [])
+
+  function draw() {
+    const i = Math.floor(Math.random() * cards.length)
+    const randomCard = cards[i]
+
+    setCards(cards.filter(e => { return e.phoneNumber !== randomCard.phoneNumber }))
+    setDrawnCards([...drawnCards, randomCard])
+  }
+
   return (
     <div className="App">
+      <button onClick={draw}>Draw</button>
+      <BusinessCard />
     </div>
   );
 }
-
-export default App;
---
+++ learning-react/random-card-draw/src/component/BusinessCard.tsx
@@ -0,0 +1,6 @@
+
+export default function BusinessCard() {
+    return (
+        <div>Business Card</div>
+    )
+}
