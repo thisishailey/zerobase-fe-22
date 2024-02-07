6be68acdd467e47684809c6cdd6e5fa4e943c86b
Hailey Kim <kfbkhw@gmail.com>
2024-02-07 15:59:18 +0900

    starting calculator project with ts react vite

---
+++ learning-react/calculator/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+    return (
+        <div className="wrap">
+            <Calculator />
+        </div>
+    );
+}
+
+export default App;
