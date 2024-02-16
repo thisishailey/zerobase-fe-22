f817cb910c46bb944d8f7822f9c5fb5e9f5015fe
Hailey Kim <kfbkhw@gmail.com>
2024-02-16 20:42:14 +0900

    learn fetch api

--- learning-react/react-router-dom/src/components/BlogPage.tsx
+++ learning-react/react-router-dom/src/components/BlogPage.tsx
@@ -1,7 +1,46 @@
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+interface Data {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
 
 export default function BlogPage() {
     const navigate = useNavigate();
+    const [data, setData] = useState<Data[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch(
+                'https://jsonplaceholder.typicode.com/posts'
+            );
+            const data = await res.json();
+            return data;
+        }
+
+        fetchData().then(
+            (data) => {
+                setData(data);
+            },
+            (err) => console.error(err)
+        );
+    }, []);
+
+    function toggleBody(body: string, id: number) {
+        const postElement = document.getElementById('post' + id.toString());
+        let postBody = postElement?.querySelector('.postBody');
+        if (postBody) {
+            postElement!.removeChild(postBody);
+        } else {
+            postBody = document.createElement('p');
+            postBody.classList.add('postBody');
+            postBody.innerHTML = body;
+            postElement?.appendChild(postBody);
+        }
+    }
 
     return (
         <>
@@ -13,6 +52,23 @@ export default function BlogPage() {
                 Home
             </button>
             <h1>BlogPage</h1>
+            <div className="postWrap">
+                {data.map((e) => (
+                    <div
+                        className="post"
+                        id={'post' + e.id.toString()}
+                        key={e.id}
+                    >
+                        <div
+                            className="postTitle"
+                            onClick={() => toggleBody(e.body, e.id)}
+                        >
+                            <span>{e.id}</span>
+                            <h2>{e.title}</h2>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
