57f8961bdc834ecae9071e696656d2d645f598d2
Hailey Kim <kfbkhw@gmail.com>
2024-02-22 16:03:05 +0900

    finish the Main Page and seperate Header as a component

---
+++ react-mbti-test/src/components/Header.tsx
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function Header() {
+    return (
+        <NavHeader>
+            <Link to={''}>
+                <Heading>MeowBTI Test</Heading>
+            </Link>
+            <Nav>
+                <Link to={'test'}>
+                    <NavItem>Test</NavItem>
+                </Link>
+                <Link to={'result'}>
+                    <NavItem>Result</NavItem>
+                </Link>
+            </Nav>
+        </NavHeader>
+    );
+}
+
+const NavHeader = styled.div`
+    position: sticky;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 20px 54px;
+    color: #fefbf6;
+    background-color: #fba834;
+`;
+
+const Heading = styled.h1`
+    font-family: 'Anta', sans-serif;
+    font-size: 30px;
+`;
+
+const Nav = styled.nav`
+    display: flex;
+    gap: 30px;
+`;
+
+const NavItem = styled.div`
+    width: 110px;
+    padding: 6px;
+    border: 1.2px solid #ffffff;
+    border-radius: 6px;
+    font-family: 'Anta', sans-serif;
+    font-size: 20px;
+    font-weight: 700;
+    text-align: center;
+    transition: all 300ms;
+
+    &:hover {
+        border-color: #ffbb64;
+        color: #ffbb64;
+        background-color: #ffffff;
+    }
+`;
--- react-mbti-test/src/pages/MainPage.tsx
+++ react-mbti-test/src/pages/MainPage.tsx
@@ -1,23 +1,25 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Header from '../components/Header';
 
 export default function Main() {
     return (
         <Section className="mainPage">
-            <Header>
-                <Link to={''}>
-                    <Heading>MeowBTI Test</Heading>
-                </Link>
-                <Nav>
-                    <Link to={'test'}>
-                        <NavItem>Take the Test</NavItem>
-                    </Link>
-                    <Link to={'result'}>
-                        <NavItem>View Result</NavItem>
-                    </Link>
-                </Nav>
-            </Header>
+            <Header />
             <Title>Find the cat of your destiny!</Title>
+            <ImageBackground>
+                <Image></Image>
+            </ImageBackground>
+            <Description>
+                Discover the perfect cat that matches your personality <br />
+                based on the MBTI(Myers–Briggs Type Indicator) personality
+                types.
+            </Description>
+            <StartTest>
+                <Link to={'test'}>
+                    <StartTestButton>TAKE THE TEST NOW!</StartTestButton>
+                </Link>
+            </StartTest>
         </Section>
     );
 }
@@ -27,35 +29,61 @@ const Section = styled.section`
     min-height: 100vh;
 `;
 
-const Header = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    padding: 20px 54px;
-    color: #fefbf6;
-    background: #fba834;
-`;
-
-const Heading = styled.h1`
-    font-size: 30px;
-`;
-
-const Nav = styled.nav`
-    display: flex;
-    gap: 36px;
-    font-size: 20px;
-    font-weight: 500;
-`;
-
-const NavItem = styled.span`
-    padding: 10px;
-`;
-
 const Title = styled.h2`
     width: 100%;
-    padding: 40px;
-    font-size: 40px;
+    padding: 60px 0;
+    font-size: 48px;
     text-align: center;
     color: #ff8911;
+    background-color: #ffffff;
+`;
+
+const ImageBackground = styled.div`
+    display: flex;
+    justify-content: center;
+    background-image: linear-gradient(#ffffff, #ffbb64);
+`;
+
+const Image = styled.div`
+    width: 480px;
+    height: 480px;
+    border-radius: 240px;
+    margin: 20px 0;
+    background-image: url(/cat.png);
+    background-position: 60% 40%;
+`;
+
+const Description = styled.div`
+    width: 100%;
+    padding: 100px;
+    font-size: 30px;
+    font-weight: 600;
+    color: #fefbf6;
+    background-color: #ffbb64;
+`;
+
+const StartTest = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    padding: 40px 0 200px;
+    background-color: #ffbb64;
+`;
+
+const StartTestButton = styled.button`
+    width: 300px;
+    height: 60px;
+    border-radius: 12px;
+    font-family: 'Anta', sans-serif;
+    font-size: 24px;
+    font-weight: 600;
+    color: #ffbb64;
+    background-color: #ffffff;
+    transition: all 300ms;
+    cursor: pointer;
+
+    &:hover {
+        color: #ffffff;
+        background-color: #ff8911;
+    }
 `;
