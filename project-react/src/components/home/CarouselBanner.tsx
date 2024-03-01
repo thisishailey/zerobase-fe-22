c3745b08014b58569d2e18c3d7ede3f0d955d764
Hailey Kim <kfbkhw@gmail.com>
2024-03-01 10:03:36 +0900

    edit: CarouselBanner - use Array.map() and center the images using margin

--- project-react/src/components/home/CarouselBanner.tsx
+++ project-react/src/components/home/CarouselBanner.tsx
@@ -6,36 +6,29 @@ import beverages from '@/assets/beverages.jpg';
 import books from '@/assets/books.jpg';
 import grocery from '@/assets/grocery.jpg';
 import mall from '@/assets/mall.jpg';
-import shopping from '@/assets/shopping.jpg';
 
 export default function CarouselBanner() {
+    const carouselItems = [bakery, beverages, books, grocery, mall];
+
     return (
         <>
             <div className="w-full max-h-[600px] mt-1 bg-neutral-400">
                 <Carousel className="w-full h-full" pause={'hover'}>
-                    <CarouselItem>
-                        <Image
-                            src={bakery}
-                            alt={'bakery'}
-                            height={600}
-                            priority
-                        />
-                    </CarouselItem>
-                    <CarouselItem>
-                        <Image src={beverages} alt={'beverages'} height={600} />
-                    </CarouselItem>
-                    <CarouselItem>
-                        <Image src={books} alt={'books'} height={600} />
-                    </CarouselItem>
-                    <CarouselItem>
-                        <Image src={grocery} alt={'grocery'} height={600} />
-                    </CarouselItem>
-                    <CarouselItem>
-                        <Image src={mall} alt={'mall'} height={600} />
-                    </CarouselItem>
-                    <CarouselItem>
-                        <Image src={shopping} alt={'shopping'} height={600} />
-                    </CarouselItem>
+                    {carouselItems.map((item, i) => {
+                        return (
+                            <>
+                                <CarouselItem>
+                                    <Image
+                                        className="my-0 mx-auto"
+                                        src={item}
+                                        alt={'carousel item'}
+                                        height={600}
+                                        priority={i === 0}
+                                    />
+                                </CarouselItem>
+                            </>
+                        );
+                    })}
                 </Carousel>
             </div>
         </>
