e5e113c955a9bd509194b5b33288b25d5416ccef
Hailey Kim <kfbkhw@gmail.com>
2024-03-02 12:26:14 +0900

    update: Carousel - new pictures & Image sizes prop

--- project-react/src/components/home/CarouselBanner.tsx
+++ project-react/src/components/home/CarouselBanner.tsx
@@ -1,23 +1,23 @@
 import Image from 'next/image';
 import Carousel from 'react-bootstrap/Carousel';
 import CarouselItem from 'react-bootstrap/CarouselItem';
-import bakery from '@/assets/bakery.jpg';
-import beverages from '@/assets/beverages.jpg';
-import books from '@/assets/books.jpg';
-import grocery from '@/assets/grocery.jpg';
-import mall from '@/assets/mall.jpg';
+import item1 from '@/assets/carousel/carousel1.jpg';
+import item2 from '@/assets/carousel/carousel2.jpg';
+import item3 from '@/assets/carousel/carousel3.jpg';
+import item4 from '@/assets/carousel/carousel4.jpg';
+import item5 from '@/assets/carousel/carousel5.jpg';
 
 export default function CarouselBanner() {
-    const carouselItems = [bakery, beverages, books, grocery, mall];
+    const carouselItems = [item1, item2, item3, item4, item5];
 
     return (
         <>
-            <div className="w-full h-[600px] mt-1 bg-neutral-400">
+            <div className="w-full h-[400px] sm:h-[500px] md:h-[600px] mt-1 bg-neutral-400">
                 <Carousel className="w-full h-full" pause={'hover'}>
                     {carouselItems.map((item, i) => {
                         return (
                             <CarouselItem
-                                className="relative w-full h-[600px]"
+                                className="relative w-full h-[400px] sm:h-[500px] md:h-[600px]"
                                 key={i}
                             >
                                 <Image
@@ -25,6 +25,7 @@ export default function CarouselBanner() {
                                     src={item}
                                     alt={'carousel item'}
                                     fill={true}
+                                    sizes="100vw, 1280px"
                                     priority={i === 0}
                                 />
                             </CarouselItem>
