da29ac8a3cdb3f7b82871803551f679598f377d2
Hailey Kim <kfbkhw@gmail.com>
2024-03-07 17:35:45 +0900

    edit: move ProductDetailCart to '/product' from '/common' create: Product Breadcrumbs update: show breadcrumbs in product detail page & change params of detail card

--- project-react/src/app/product/detail/[id]/page.tsx
+++ project-react/src/app/product/detail/[id]/page.tsx
@@ -1,7 +1,8 @@
 import Wrap from '@/components/common/template/wrap';
-import { getProductsData } from '@/api/products';
+import { getProductsData, getProductData } from '@/api/products';
 import type IProduct from '@/types/productData';
-import ProductDetailCard from '@/components/common/ProductDetailCard';
+import ProductBreadcrumbs from '@/components/product/ProductBreadcrumbs';
+import ProductDetailCard from '@/components/product/ProductDetailCard';
 
 export async function generateStaticParams() {
     const products: IProduct[] = await getProductsData();
@@ -11,12 +12,21 @@ export async function generateStaticParams() {
     }));
 }
 
-export default function ProductDetail({ params }: { params: { id: string } }) {
+export default async function ProductDetail({
+    params,
+}: {
+    params: { id: string };
+}) {
     const { id } = params;
+    const product: IProduct = await getProductData(id);
 
     return (
         <Wrap>
-            <ProductDetailCard id={id} />
+            <ProductBreadcrumbs
+                category={product.category}
+                name={product.title}
+            />
+            <ProductDetailCard product={product} />
             <div className="h-20"></div>
         </Wrap>
     );
